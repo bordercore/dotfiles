b4c537cdefd4639fafe7d1d191f49cc78461dc52
F. Jerrell Schivers <jerrell@bordercore.com>
2025-12-28 19:23:50 -0500

    Add zsh to install script and quote all $HOME paths for safety

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-stow -t $HOME bash
-stow -t $HOME/.emacs.d/ emacs
-stow -t $HOME git
-stow -t $HOME less
-stow -t $HOME psql
-stow -t $HOME tmux
-stow -t $HOME top
-stow -t $HOME/bin bash-osx
+stow -t "$HOME" bash
+stow -t "$HOME" zsh
+stow -t "$HOME/.emacs.d/" emacs
+stow -t "$HOME" git
+stow -t "$HOME" less
+stow -t "$HOME" psql
+stow -t "$HOME" tmux
+stow -t "$HOME" top
+stow -t "$HOME/bin" bash-osx
